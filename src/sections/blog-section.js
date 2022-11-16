e7baeee11b63654e9dbf0dc95b8998384eb4ae9a
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-16 12:55:16 +0600

    Update blog-section.js

--- src/sections/blog-section.js
+++ src/sections/blog-section.js
@@ -52,22 +52,22 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1310 },
     items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    slidesToSlide: 3,
   },
   laptop: {
     breakpoint: { max: 1310, min: 1024 },
     items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    slidesToSlide: 3,
   },
   tablet: {
     breakpoint: { max: 1024, min: 640 },
     items: 2,
-    slidesToSlide: 2, // optional, default to 1.
+    slidesToSlide: 2,
   },
   mobile: {
     breakpoint: { max: 639, min: 0 },
     items: 1,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1,
   },
 };
 
